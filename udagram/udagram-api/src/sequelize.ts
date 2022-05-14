35f44ba77cd6a295113cfaa03fe0212306ddc5f5
Ahmed Magdy <ama@luftborn.com>
2022-05-15 00:00:14 +0200

    some minor changes

--- udagram/udagram-api/src/sequelize.ts
+++ udagram/udagram-api/src/sequelize.ts
@@ -2,5 +2,5 @@ import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
 export const sequelize = new Sequelize(
-    "postgres://postgres:postgres@database-1.cytqbal9ytlh.us-east-1.rds.amazonaws.com:5432/postgres"
+    `postgres://${config.username}:${config.password}@${config.host}:${config.pg_port}/${config.database}`
 );
